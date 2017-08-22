52ce2fdc16bfe90f91f7e67feae358f4f5175e4e
ying xin <395488553@qq.com>
2017-08-22 18:04:18 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='ying xin',
+    author_email='395488553@qq.com',
+    url='qq.com',
+    py_modules=['vsearch'],
+)
