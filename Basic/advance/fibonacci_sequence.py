36ca92e1a831328e2e60fe515d0e6087b0168f4a
ying xin <395488553@qq.com>
2020-06-23 15:36:36 +0800

    Create fibonacci_sequence.py

---
+++ Basic/advance/fibonacci_sequence.py
@@ -0,0 +1,33 @@
+'generator 生成器'
+
+'斐波那契数列 1,1,2,3,5,8,13,21,34,...'
+
+def fib(max):
+    n,a,b = 0,0,1
+    while n<max:
+        print(b)
+        a,b=b,a+b
+        n=n+1
+    return 'done'
+
+def fibGenerator(max):
+    n,a,b = 0,0,1
+    while n<max:
+        yield b
+        a,b=b,a+b
+        n=n+1
+    return 'done'
+
+'输出1'
+for n in fibGenerator(6):
+    print(n)
+
+'输出2'
+f1=fibGenerator(6)
+while True:
+    try:
+        x=next(f1)
+        print('f: ',x)
+    except StopIteration as e:
+        print('Generator return value: ', e.value)
+        break
